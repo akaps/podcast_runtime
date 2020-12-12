59e6ec9dc6133e5693db644b10be10cbd324c20d
enrrari <enrrari@protonmail.com>
2020-12-12 20:53:07 +0000

    Remove leftover code from copied file

--- SFBOlength.py
+++ SFBOlength.py
@@ -6,15 +6,8 @@ from subprocess import Popen, PIPE
 import time
 
 def main():
-    # Dump the Spotify's page for SFBO into a variable
-    #sfbo = subprocess.check_output('w3m -dump https://open.spotify.com/show/5z3G1urniqVGKCNZSQXhX0', shell=True)
-
-
     # Uncomment to print page content
     #print(sfbo)
-
-    # Convert page content on variable into string
-    #sfbo = str(sfbo)
 
     times = []
     i = 0
@@ -32,29 +25,5 @@ def main():
             line = re.sub(r'(^[ \t]+|[ \t]+(?=:))', '', line, flags=re.M)
             print(line)
 
-"""
-    # Convert BPM into int
-    # sfbo = int(sfbo)
-
-    h = 0
-    m = 0
-    s = 0
-    for i in range(len(times)):
-        
-        if(times[i].count(":") == 2):
-            h += int(times[i].split(":")[0])
-            m += int(times[i].split[":"][1].lstrip().split(":")[2])
-            s += int(times[i].split[":"][2].lstrip())
-        else:
-            m += int(times[i].split(":")[0])
-            s += int(times[i].split[":"][2].lstrip())
-
-        print(times[i])
-
-    print(h, m, s)
-
-    # Return BPM
-    #return BPM
-    """
 main()
 
